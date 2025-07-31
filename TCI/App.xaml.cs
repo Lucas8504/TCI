6c57220fc95dc824b1ba435c8f72a29ade84673d
Lucas Araujo <lsa77793@gmail.com>
2025-07-30 22:32:16 -0300

    Agregar archivos de proyecto.

---
+++ TCI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using TCI.MVVM.Views;
+
+namespace TCI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new TCIView();
+        }
+    }
+}
---
+++ TCI/MVVM/Views/TCIView.xaml.cs
@@ -0,0 +1,9 @@
+namespace TCI.MVVM.Views;
+
+public partial class TCIView : ContentPage
+{
+	public TCIView()
+	{
+		InitializeComponent();
+	}
+}
