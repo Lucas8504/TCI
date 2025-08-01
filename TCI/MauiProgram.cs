f70613b8e36dd962f97fbc5ca9e1c013d0ed73fe
Lucas Araujo <lsa77793@gmail.com>
2025-08-01 11:12:49 -0300

    Añadir soporte para Syncfusion en la aplicación

--- TCI/MauiProgram.cs
+++ TCI/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Syncfusion.Maui.Core.Hosting;
 
 namespace TCI
 {
@@ -9,6 +10,7 @@ namespace TCI
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .ConfigureSyncfusionCore()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
