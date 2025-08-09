4a8fa83d7200e144b10cb8e321d5aa6d34ec2a50
Lucas Araujo <lsa77793@gmail.com>
2025-08-08 22:44:55 -0300

    construcion de clase TCI en Models

---
+++ TCI/MVVM/Models/TCI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCI.MVVM.Models
+{
+    class TCI
+    {
+        public int Temp { get; set; }
+
+        public string Grav { get; set; }
+
+    }
+}
