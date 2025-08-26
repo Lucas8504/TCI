aef27535fd5dc85a4dfa1dfb22da9d0b742511a2
Lucas Araujo <lsa77793@gmail.com>
2025-08-26 20:54:09 -0300

    añadiendo  el conversor TempToColorConverter.cs

---
+++ TCI/MVVM/ViewModels/TempToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace TCI.Converters
+{
+    public class TempToColorConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is double temp)
+            {
+                if (temp < 36.5) return Color.FromArgb("#4CAF50"); // verde
+                if (temp < 38) return Color.FromArgb("#FFEB3B");    // amarillo
+                if (temp < 39) return Color.FromArgb("#FF9800");    // naranja
+                return Color.FromArgb("#F44336");                   // rojo
+            }
+            return Colors.Gray;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
